0373374dbfb87bf60b92ea9e16065f0fd83daf8d
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-11-23 19:41:29 +0300

    Create User.js

---
+++ Src/Commands/User.js
@@ -0,0 +1,53 @@
+const config = global.config;
+const client = global.client;
+const MemberData = require('../schema/UserLenght');
+const ChannelData = require('../schema/Channel');
+const MessageChannel = require('../schema/MessageChannel');
+const MessageData = require('../schema/MessageData');
+const { MessageEmbed } = require("discord.js");
+ class Commands {
+  constructor() {
+    this.name = "user";
+    this.description = "üyesinin ses ve chat istatistikleri gösterir";
+    this.options = [
+        {
+            type: 6,
+            description: `lütfen bir kullancı seçin`,
+            name: "users",
+        }
+    ];
+  }
+  async execute(ctx) {
+    let embed = new MessageEmbed()
+    const Member = ctx.options._hoistedOptions.find(t => t.name == "users")
+    .setAuthor({name: ctx.guild.name,iconURL: ctx.guild.iconURL({ dynamic: true }),})
+    .setFooter({text: config.footer.toString(),iconURL: ctx.user.avatarURL({ dynamic: true })})
+    .setColor("RANDOM");
+    let MessageDb = await MessageData.findOne({ GuildId: ctx.guild.id, MemberId: Member.value,   })
+    let MessageChannelDb = await MessageChannel.find({ GuildId: ctx.guild.id, MemberId: Member.value })
+    let ChannelDb = await ChannelData.find({ GuildId: ctx.guild.id, MemberId: Member.value }) 
+    let MemberDb = await MemberData.findOne({ GuildId: ctx.guild.id, MemberId: Member.value })
+    if (MessageDb && MemberDb) {
+        const contenttwo = `${MessageChannelDb.map( (t, index) => ` \`${index+1}\` <#${t.Channel}> \`${t.Point}\` `).join("\n")}`
+        const content = `${ChannelDb.map((x, index) => `\`${index+1}\` <#${x.Channel}> \`${Cover(x.VoiceTime)}\` `).join("\n")}`
+        ctx.reply({ embeds: 
+          [embed.setDescription(` <@${Member.value}> üyesinin ses ve chat istatistikleri 
+          \n Toplam Mesaj Sayısı \`${MessageDb.Point}\` 
+          Toplam Ses Sayısı \`${Cover(MemberDb.VoiceTime)}\`
+          \n **Ses** 
+          ${content} 
+          \n **Mesaj** 
+          ${contenttwo}`)] })
+    } else {
+        ctx.reply("Hiç bir veriniz bulunmuyor")
+    }
+}
+}
+
+module.exports = Commands;
+ 
+function Cover(millis) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+}
