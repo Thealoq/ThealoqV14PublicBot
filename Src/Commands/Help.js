7473c9a53b3c5f2c2bbce8ba1c8edf41be235730
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-11-23 23:38:17 +0300

    Update Help.js
    xd

--- Src/Commands/Help.js
+++ Src/Commands/Help.js
@@ -15,7 +15,7 @@ const { MessageEmbed } = require("discord.js");
     .setFooter({text: config.footer.toString(),iconURL: ctx.client.user.avatarURL({ dynamic: true })})
     .setColor("RANDOM");
     commands.map((x, i) => console.log(x.on))
-    const content = `${commands.map((x, index) => ` ${x.on ? "✅": "❌"} \`/${x.name}\` Açıklama \`${x.description}\` `).join("\n")}`
+    const content = `${commands.map((x, index) => ` ✅ \`/${x.name}\` Açıklama \`${x.description}\` `).join("\n")}`
     ctx.reply({ embeds: [embed.setDescription(`${content}`)]})
 }
 }
