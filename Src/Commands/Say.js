230d441da0b1071e59c21ff7f73a5c100f9a405d
Thealoq <79800238+Thealoq@users.noreply.github.com>
2023-02-03 19:53:18 +0300

    Create Say.js

---
+++ Src/Commands/Say.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { EmbedBuilder, Attachment, PermissionsBitField } = require("discord.js");
+const fetch = require('node-fetch');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("info")
+        .setDescription("show guild info")
+        .addStringOption((option) => option.setRequired(false).setName('id').setDescription('Just enter the id')),
+    run: async (ctx) => {
+        let embed = new EmbedBuilder()
+        .setColor(`RANDOM`)
+      ctx.reply({
+        embeds: [embed.setDescription(`
+      \`❯\` Sunucunda Toplam ${ctx.guild.members.cache.filter(s => s.voice.channel).size} Kişi Ses Kanalında Aktif 
+      \`❯\` Sunucu da şuan da toplam ${ctx.guild.memberCount} üye var 
+       `)]
+      })
+        }
+}
